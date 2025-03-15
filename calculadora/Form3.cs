1679f93f1562bed2f7bbcd312ec5fb99c964b061
FELLIPE COSTA PAZZOTTO <fellipe.cpazzotto@senacsp.edu.br>
2025-03-14 22:10:39 -0300

    Finalizando as três calculadoras

--- calculadora/Form3.cs
+++ calculadora/Form3.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.Windows.Forms.DataFormats;
-
-namespace calculadora
+﻿namespace calculadora
 {
     public partial class calculadora_circunferencias : Form
     {
