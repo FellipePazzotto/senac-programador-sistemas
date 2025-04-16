a5c46724e00b58cf645712410125a7ed7fb39fdb
FELLIPE COSTA PAZZOTTO <fellipe.cpazzotto@senacsp.edu.br>
2025-04-16 18:45:20 -0300

    Voltei para o meu computador

--- ListaAtividades/obj/Release/net8.0-windows/ListaAtividades.AssemblyInfo.cs
+++ ListaAtividades/obj/Release/net8.0-windows/ListaAtividades.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListaAtividades")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb4c29d7b61ec312e74b6ff424981c902859e20")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListaAtividades")]
